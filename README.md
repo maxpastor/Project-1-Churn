3ec7436795d9c76bb6844767a6b178fe66a60b6f
Maxence Pastor <23473052+maxpastor@users.noreply.github.com>
2023-02-23 16:40:54 +0000

    Pushing what I have now

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Project 1: Churn
- Project 1 of the 1 model a week series. This one tackles customer churn. 
+ Project 1 of the **1 Model a Week** series. This one tackles customer churn. 
+
+ ## Disclamer:
+Even though I work at AWS, this code is a personal project and is not developped or provided by/on-behalf of AWS. 
+The code might have errors or might not follow best practices. 
+
+Running this code on your AWS Account will trigger costs (Compute, Storage and Networking) so use at your own risk. 
+
+## How-To use:
+
+The main notebook contains a simple Feed Forward model that is supposed to output if a customer is going to churn or not. 
+The goal is not to have extremely good results, but to show how to implement such a solution. If you fancy experimenting with XGboost to solve this problem, you will see that it actually performs better out of the box... And that's alright. 
+
+The notebook is in 3 Parts (I will document them more later, or at least I'll try):
+
+- 1: We run the training on the Notebook (not best practice but good for quick experimentation and development)
+- 2: We run the training using a Sagemaker Training Job on a simple CPU machine (no need for a fancy GPU)
+- 3: We run a Hyperparameter tuning Job on Sagemaker where we modulate the learning rate, num of neurons per layer, dropout and number of Epochs, to see if we get a better AUC. 
+
+The code does not have at this moment any Inference code. It would not be so difficult to develop but it was not my primary focus. 
+It doesn't even save the model. 
+So you will see how the training did but you want be able to do anything with it at the moment. 
+
+I will implement that last bit when I have the time. 
+
+Feel free to suggest edits. The main goal of this repo and this new series is to force me to write more Pytorch code, as I was a Tensorflow instructor in the past, I need to get more familiar with it. 
+
+XGboost instructions and data can be found here: https://sagemaker-examples.readthedocs.io/en/latest/introduction_to_applying_machine_learning/xgboost_customer_churn/xgboost_customer_churn.html
+
+Cheers !
+
+
---
+++ train.py
@@ -0,0 +1,160 @@
+#We need 4 arguments: num_neurons, learning_rate, droppout, num_epochs
+#This script will then be passed to Sagemaker for training
+import argparse
+import torch
+import pandas as pd
+import torch.nn.functional as F
+import torch.nn as nn
+from torch import optim
+import logging
+import sys
+import os
+from sklearn.metrics import roc_auc_score
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+#from tqdm import tqdm
+def roc_auc(y_true, y_pred):
+    
+    y_true = y_true.detach().cpu().numpy()
+    y_pred = y_pred.detach().cpu().numpy()
+    auc = roc_auc_score(y_true, y_pred)
+
+
+    return auc
+
+def main(args):
+    train_df = pd.read_csv(args.data_dir + "/train.csv")
+    validation_df = pd.read_csv(args.test_dir + "/validation.csv")
+    num_features = train_df.shape[1]-1
+    num_neurons = args.neurons
+    dropout_value = args.dropout
+    y_train = train_df.iloc[:,0]
+    x_train = train_df.iloc[:, 1:]
+
+    y_validation = validation_df.iloc[:,0]
+    x_validation = validation_df.iloc[:,1:]
+
+    class FeedForwardModel(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.network = nn.Sequential(
+                nn.Linear(num_features, num_neurons),
+                nn.LeakyReLU(),
+                nn.Linear(num_neurons, num_neurons),
+                nn.Dropout(p=dropout_value),
+                nn.LeakyReLU(),
+                nn.Linear(num_neurons, num_neurons),
+                nn.Dropout(p=dropout_value),
+                nn.LeakyReLU(),
+                nn.Linear(num_neurons, num_neurons),
+                nn.Dropout(p=dropout_value),
+                nn.LeakyReLU(),
+                nn.Linear(num_neurons, num_neurons),
+                nn.Dropout(p=dropout_value),
+                nn.LeakyReLU(),
+                nn.Linear(num_neurons, num_neurons),
+                nn.Dropout(p=dropout_value),
+                nn.LeakyReLU(),
+                nn.Linear(num_neurons, 1),
+                nn.Sigmoid()
+        )
+        def forward(self, x: torch.tensor):
+            return self.network(x)
+
+    def train(x, y, model, opt, loss_fn, epochs=args.epochs):
+        train_loss_values = []
+        train_auc_values = []
+
+        for epoch in range(epochs):
+            model.train()
+            pred = model(x)
+            loss = loss_fn(pred, torch.unsqueeze(y, 1))
+            loss.backward()
+            opt.step()
+            opt.zero_grad()
+            train_loss_values.append(loss.item())
+            
+            if epoch % 100 == 0:
+                train_auc = roc_auc(y, pred)
+                train_auc_values.append(train_auc)
+                logger.info(
+                    "Train Epoch: {} Loss: {:.6f} , AUC: {}".format(
+                        epoch,
+                        loss.item(),
+                        train_auc
+                    )
+                )
+                
+
+        return train_loss_values, train_auc_values
+
+    def validate(x,y, model, loss_fn):
+        with torch.inference_mode():
+            model.eval()
+            pred = model(x)
+            loss = loss_fn(pred, torch.unsqueeze(y, 1))
+            auc = roc_auc(y, pred)
+            logger.info(
+                "Test set: Average loss: {:.4f}, AUC: {}\n".format(
+                    loss, auc
+                )
+            )
+            return loss, auc
+    device = torch.device("cpu")
+    #Mapping our features and labels as tensors
+    x_train_tensor = torch.tensor(x_train.values, dtype=torch.float)
+    y_train_tensor = torch.tensor(y_train.values, dtype=torch.float)
+
+    x_validation_tensor = torch.tensor(x_validation.values, dtype=torch.float)
+    y_validation_tensor = torch.tensor(y_validation.values, dtype=torch.float)
+
+    x_train_tensor = x_train_tensor.to(device)
+    y_train_tensor = y_train_tensor.to(device)
+
+    x_validation_tensor = x_validation_tensor.to(device)
+    y_validation_tensor = y_validation_tensor.to(device)
+    model = FeedForwardModel()
+    model.to(device)
+    loss_fn = nn.BCELoss()
+    opt = optim.Adam(params=model.parameters(), lr=args.lr)
+    losses, aucs= train(x_train_tensor, y_train_tensor, model, opt, loss_fn)
+    validate(x_validation_tensor, y_validation_tensor, model, loss_fn)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--neurons",
+        type=int,
+        default=1024,
+        metavar="N",
+        help="The number of neurons per layer",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int, 
+        default=1000,
+        metavar="N",
+        help="The number of training iterations before we stop",
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=0.001,
+        metavar="LR",
+        help="Learning rate of the model (default 0.001)",
+    )
+    parser.add_argument(
+        "--dropout",
+        type=float,
+        default=0.2,
+        metavar="DP",
+        help="The dropout applied to the model, helps reduce overfitting",
+    )
+    parser.add_argument("--data-dir", type=str, default=os.environ["SM_CHANNEL_TRAIN"])
+    parser.add_argument("--test-dir", type=str, default=os.environ["SM_CHANNEL_VALIDATE"])
+    main(parser.parse_args())
